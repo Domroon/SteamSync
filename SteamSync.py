357239e5118401b19775c2cbcc92c90a5090b165
Dominik Haeusser <haeusser-dominik@web.de>
2021-02-03 12:29:52 +0100

    add is_running

--- SteamSync.py
+++ SteamSync.py
@@ -5,39 +5,13 @@ from pathlib import Path
 from time import sleep
 import os
 import time
+import psutil
 
 BASE_PATH = Path("C:/Users/haeus/workspace")
 SOURCE_FILE_PATH = BASE_PATH / "Testordner1" / "Datei.txt"
 TARGET_FILE_PATH = BASE_PATH / "Testordner2" / "Datei.txt"
 
 PROGRAM_NAME = "OUTLOOK.EXE"
-
-
-def is_running(search_term):
-    result = subprocess.run(
-        ["tasklist"],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        check=True,
-    )
-    return search_term in result.stdout
-
-
-def main2():
-    while True:
-        if is_running(PROGRAM_NAME):
-            print("Programm läuft")
-
-            while is_running(PROGRAM_NAME):
-                print("Warte auf Beendigung")
-                sleep(0.1)
-
-            shutil.copy(SOURCE_FILE_PATH, TARGET_FILE_PATH)
-            print("Datei wurde kopiert")
-        else:
-            print("Programm läuft nicht")
-
-        sleep(0.1)
 
 def source_is_newer(SOURCE_FILE_PATH, TARGET_FILE_PATH):
     statinfo_source = os.stat(SOURCE_FILE_PATH)
@@ -50,11 +24,35 @@ def source_is_newer(SOURCE_FILE_PATH, TARGET_FILE_PATH):
     else:
         return False
 
+def is_running(search_term):
+    for proc in psutil.process_iter(['pid', 'name']):
+        process_name = proc.info["name"]
+
+        if process_name == search_term:
+            return True
+
 def main():
-    if source_is_newer(SOURCE_FILE_PATH, TARGET_FILE_PATH):
-        print('Quelldatei ist neuer')
-    else:
-        print('Quelldatei ist älter')
+    while True:
+        if is_running(PROGRAM_NAME):
+            print("Programm läuft")
+
+            while is_running(PROGRAM_NAME):
+                print("Warte auf Beendigung")
+                sleep(0.1)
+
+
+            if source_is_newer(SOURCE_FILE_PATH, TARGET_FILE_PATH):
+                print('Quelldatei ist neuer')
+                shutil.copy(SOURCE_FILE_PATH, TARGET_FILE_PATH)
+                print("Datei wurde kopiert")
+            else:
+                print('Quelldatei ist älter oder auf dem gleichen Stand')
+                print('Datei wurde nicht kopiert')
+            
+        else:
+            print("Programm läuft nicht")
+
+        sleep(0.1)   
 
 if __name__ == "__main__":
     main()
